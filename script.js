15e42f8a8517355b66c50f63dd8f1a8e05a9ec43
Matt Frank <matthew.i.frank@gmail.com>
2017-01-03 16:03:07 -0600

    first attempt at longpress

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-/*if (typeof jQuery != 'undefined') { window.alert("jQuery library is loaded!");}else{ window.alert("jQuery library is not found!");} */
-
-
-
 $(document).ready(function(){
     var testNumLength = function(number) {
         if (number.length > 9) {
@@ -16,40 +12,50 @@ $(document).ready(function(){
     var newnumber = "";
     var operator = "";
     var totaldiv = $("#total");
+    var shiftKey = false;
+    var myTimer;
     totaldiv.text("0");
-    $(".digit").click(function(){
-	    window.navigator.vibrate(30);
-		number += $(this).text();
-		totaldiv.text(number);
-		testNumLength(number);
+    $(".digit").onmousedown(function(){
+	window.navigator.vibrate(30);
+	myTimer = setTimeout(function(){
+	    shiftKey = !shiftKey;
+	    window.navigator.vibrate(20);
+	}, 500);
+    }).onmouseleave(function(){
+	clearTimeout(myTimer);
+    }).onmouseup(function(){
+	number += $(this).text();
+	if (shiftKey) { number += "x"; }
+	totaldiv.text(number);
+	testNumLength(number);
     });
-    $("#operators a").not("#equals").click(function(){
-		operator = $(this).text();
-		newnumber = number;
-		number = "";
-		totaldiv.text("0");
+/*    $("#operators a").not("#equals").click(function(){
+	operator = $(this).text();
+	newnumber = number;
+	number = "";
+	totaldiv.text("0");
     });
     $("#clear,#clearall").click(function(){
-		number = "";
-		totaldiv.text("0");
-		if ($(this).attr("id") === "clearall") {
-			newnumber = "";
-		}
-    });
+	number = "";
+	totaldiv.text("0");
+	if ($(this).attr("id") === "clearall") {
+	    newnumber = "";
+	}
+    }); */
     $("#equals").click(function(){
-		if (operator === "+"){
-			number = (parseInt(number, 10) + parseInt(newnumber,10)).toString(10);
-		} else if (operator === "-"){
-			number = (parseInt(newnumber, 10) - parseInt(number,10)).toString(10);
-		} else if (operator === "/"){
-			number = (parseInt(newnumber, 10) / parseInt(number,10)).toString(10);
-		} else if (operator === "*"){
-			number = (parseInt(newnumber, 10) * parseInt(number,10)).toString(10);
-		}
-		totaldiv.text(number);
-		testNumLength(number);
-		number = "";
-		newnumber = "";
+	if (operator === "+"){
+	    number = (parseInt(number, 10) + parseInt(newnumber,10)).toString(10);
+	} else if (operator === "-"){
+	    number = (parseInt(newnumber, 10) - parseInt(number,10)).toString(10);
+	} else if (operator === "/"){
+	    number = (parseInt(newnumber, 10) / parseInt(number,10)).toString(10);
+	} else if (operator === "*"){
+	    number = (parseInt(newnumber, 10) * parseInt(number,10)).toString(10);
+	}
+	totaldiv.text(number);
+	testNumLength(number);
+	number = "";
+	newnumber = "";
     });
 });
 
