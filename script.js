278dde67ddea1387d3be461c291e608d9ec4aa2e
wandering-logic <matthew.i.frank@gmail.com>
2017-01-03 15:41:11 -0600

    vibrate for 30ms

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
     var totaldiv = $("#total");
     totaldiv.text("0");
     $(".digit").click(function(){
-	    window.navigator.vibrate(20);
+	    window.navigator.vibrate(30);
 		number += $(this).text();
 		totaldiv.text(number);
 		testNumLength(number);
